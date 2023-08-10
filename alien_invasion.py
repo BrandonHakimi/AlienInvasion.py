d872b3b4600e28dec6e76b2ff69511b3d5b41b7a
Brandon Hakimi <hakimibrandon00@gmail.com>
2023-08-10 01:51:07 -0700

    debugging alien_invasion.py

--- alien_invasion.py
+++ alien_invasion.py
@@ -4,6 +4,7 @@ import pygame
 class AlienInvasion:
     """Overall class to manage game assets and behavior."""
 
+
     def __init__(self):
         """Initialize the game, and create game resources."""
         pygame.init()
@@ -11,9 +12,12 @@ class AlienInvasion:
         self.screen = pygame.display.set_mode((1200, 800))
         pygame.display.set_caption("Alien Invasion")
 
+        # Set the background color
+        self.bg_color = (230, 0, 0)
+
+
     def run_game(self):
         """Start the main loop for the game"""
-
         while True:
             # Watch for keyboard and mouse events.
             for event in pygame.event.get():
@@ -22,6 +26,8 @@ class AlienInvasion:
             
             # Make the most recently drawn screen visible.
             pygame.display.flip()
+
+
 
 if __name__ == '__main__':
     # Make a game instance, and run the game.
